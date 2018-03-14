5a49b6a4040b587e186e1ff3b5fec2059500e7b1
dowenb00 <34041313+dowenb00@users.noreply.github.com>
2018-03-14 16:41:37 -0500

    Add project files.

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "people";
+            string s2 = "abcdefg";
+            string s3 = "dictionary";
+            bool iso = true;
+
+            s2.ToLower();
+            
+            for (int i = 0; i < s2.Length; i++)
+            {
+                char letter = s2[i];
+                for (int x = 0; x < s2.Length; x++)
+                {
+                    if (x == i) { }
+
+                    else
+                    {
+                        if (letter == s2[x])
+                        {
+                            iso = false;                            
+                        }
+                    }
+                }
+            }
+            if (iso)
+            {
+                Console.WriteLine(s2 + " is an Isogram");
+            }
+            else
+                Console.WriteLine(s2 + "is not an Isogram");
+        }
+    }
+    
+}
